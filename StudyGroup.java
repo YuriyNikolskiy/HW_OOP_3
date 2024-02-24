b7e6cc615080fe068948c107af48bd9a251ddaaa
Yuriy Nikolskiy <bankirerbe@gmail.com>
2024-02-24 18:13:04 +0300

    first commit

---
+++ StudyGroup.java
@@ -0,0 +1,30 @@
+package hw3;
+
+public class StudyGroup {
+    private int studyGroupID;
+    private String nameGroup;
+   
+
+    public int getStudyGroupID() {
+        return studyGroupID;
+    }
+
+    public void setStudyGroupID(int studyGroupID) {
+        this.studyGroupID = studyGroupID;
+    }
+
+    public StudyGroup(int studyGroupID, String nameGroup) {
+        this.studyGroupID = studyGroupID;
+        this.nameGroup = nameGroup;
+    }
+
+    @Override
+    public String toString() {
+        return "StudyGroup [ID=" + studyGroupID + ", name=" + nameGroup + "]";
+    }
+
+    
+
+    
+
+}
